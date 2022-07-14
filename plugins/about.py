e6c71d622a6956d7922b4bf6dc50bd0020313623
Malitha Mihiranga <109048518+xMalitha@users.noreply.github.com>
2022-07-14 19:03:46 +0530

    ʀᴇɴᴀᴍᴇʀ

--- plugins/about.py
+++ plugins/about.py
@@ -4,4 +4,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command(["about"]))
 async def start(client,message):
-	await message.reply_text("🎊 Bot :- @rename_alphabot\n 👨‍💻 Creater :- <tg-spoiler>@xMalitha </tg-spoiler>\n🎁 Language :-Python3\n🎨 Library :- Pyrogram 1.4.16\n💫 Server :- Heroku")
+	await message.reply_text("🎊 Bot :- @_______\n 👨‍💻 Creater :- <tg-spoiler>@xMalitha </tg-spoiler>\n🎁 Language :-Python3\n🎨 Library :- Pyrogram 1.4.16\n💫 Server :- Heroku")
